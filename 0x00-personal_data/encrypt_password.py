eb4dbb873fcd53b92c9b293cf125e1ac7bf4cc61
Janengethe <janengethew@gmail.com>
2022-05-12 18:24:37 +0300

    bcrypt

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module encrypt_password
+"""
+import bcrypt
+
+
+def hash_password(password):
+    """
+    Returns a salted, hashed password, which is a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -34,12 +34,22 @@ print("PII_FIELDS: {}".format(len(PII_FIELDS)))
 print("End of Task 2")
 
 
-get_db = __import__('filtered_logger').get_db
+# get_db = __import__('filtered_logger').get_db
 
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT COUNT(*) FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
+# db = get_db()
+# cursor = db.cursor()
+# cursor.execute("SELECT COUNT(*) FROM users;")
+# for row in cursor:
+#     print(row[0])
+# cursor.close()
+# db.close()
+
+print("End of Task 3")
+
+hash_password = __import__('encrypt_password').hash_password
+
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
+
+print("End of Task 5")

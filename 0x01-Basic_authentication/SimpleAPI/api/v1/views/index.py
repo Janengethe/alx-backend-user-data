cedea1faf3215de66cd667f0470ba51d0fd2fc94
Janengethe <janengethew@gmail.com>
2022-05-16 15:09:24 +0300

    error forbidden handling

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unautorized() -> str:
     """endpoint to test error 401"""
     return abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """endpoint to test error 403"""
+    return abort(403)

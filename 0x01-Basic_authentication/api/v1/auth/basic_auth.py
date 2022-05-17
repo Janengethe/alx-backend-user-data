1e7dc000ad20025bc7e698030ea883dbb9e05d7d
Janengethe <janengethew@gmail.com>
2022-05-17 14:00:58 +0300

    user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,15 @@ class BasicAuth(Auth):
                     base64_authorization_header).decode('utf-8')
             except Exception:
                 return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header and isinstance(
+                decoded_base64_authorization_header,
+                str) and ":" in decoded_base64_authorization_header:
+            email, psswd = decoded_base64_authorization_header.split(":", 1)
+            return email, psswd
+        return(None, None)

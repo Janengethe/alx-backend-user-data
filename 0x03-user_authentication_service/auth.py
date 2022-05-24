1e71e6c5d74c0102c2ba8f2770f13a0d28e0c04b
Janengethe <janengethew@gmail.com>
2022-05-24 13:44:03 +0300

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """
 Module auth
 """
+import uuid
 import bcrypt
 from user import User
 from sqlalchemy.exc import NoResultFound
@@ -14,6 +15,11 @@ def _hash_password(password: str) -> bytes:
     returned bytes is a salted hash of the input password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """return a string representation of a new UUID."""
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -33,3 +39,31 @@ class Auth:
             hpassword = _hash_password(password)
             user = self._db.add_user(email, hpassword)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        locating the user by email.
+        If it exists, check the password with bcrypt.checkpw.
+        If it matches return True. In any other case, return False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return(bcrypt.checkpw(
+                password.encode("utf-8"), user.hashed_password))
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """
+        finds the user corresponding to the email,
+        generate a new UUID and store it in the database as
+        the user’s session_id, then return the session ID.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id

91d9d0777da78efb6246e0c98df9159c197d373f
Janengethe <janengethew@gmail.com>
2022-05-24 16:57:51 +0300

    reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,3 +94,19 @@ class Auth:
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Finds the user corresponding to the email.
+        If the user does not exist, it raise a ValueError exception.
+        If it exists, it generate a UUID and update the user’s reset_token
+        database field.
+        Returns the token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        uu_id = _generate_uuid()
+        self._db.update_user(user.id, reset_token=uu_id)
+        return uu_id
